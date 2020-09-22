e4cb51ca6c21fcfb33d1f0f00a7a6745b8f7fa63
home No To ri <soy_solamente@hotmail.com>
2020-09-23 01:40:39 +0300

    FHIRQ-3: add mobile menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,3 +1,19 @@
 export default {
-    name: 'Menu'
+    name: 'Menu',
+    data: () => {
+        return {
+            isShow: false
+        }
+    },
+    mounted: function () {
+        this.showMenu()
+    },
+    methods: {
+        showMenu() {
+            console.log(this.isShow)
+            if (this.isShow) this.$refs.slideMenu.classList.add("slide-menu-show")
+            else this.$refs.slideMenu.classList.remove("slide-menu-show")
+            this.isShow = !this.isShow;
+        }
+    }
 }
--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -1,19 +1,38 @@
 <template>
-  <nav class="menu">
-   <div class="menu-left">
-     <h1 class="logo">LOGO</h1>
-     <a href="#">Option 1</a>
-     <a href="#">Option 2</a>
-     <a href="#">Option 3</a>
-     <a href="#">Option 4</a>
-   </div>
-   <div class="menu-right">
-     <a href="#">Other 1</a>
-     <a href="#">Other 2</a>
-     <a href="#">Other 3</a>
-   </div>
-  </nav>
-</template>
+  <div>
+    <nav class="menu-desktop">
+      <div class="menu-left">
+        <h1 class="logo">LOGO</h1>
+        <a href="#">Option 1</a>
+        <a href="#">Option 2</a>
+        <a href="#">Option 3</a>
+      </div>
+      <div class="menu-right">
+        <a href="#">Other 1</a>
+        <a href="#">Other 2</a>
+        <a href="#">Other 3</a>
+      </div>
+    </nav>
 
+    <nav class="menu-mobile">
+      <div class="menu-burger">
+        <a class="burger-button"
+           v-on:click="showMenu()">
+          <i class="material-icons" style="font-size:36px;">
+            dehaze
+          </i>
+        </a>
+      </div>
+      <div ref="slideMenu" class="slide-menu">
+        <ul>
+          <li>Hello</li>
+          <li>Yellow</li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+
+
+</template>
 <style scoped src="./Menu.css"></style>
 <script src="./Menu.js"></script>
